5fcf76e2143e1d26680b39858c1187e1f46a2f41
marknegrillo53-byte <marknegrillo53@gmail.com>
2026-01-15 16:47:40 +0800

    Update Program.cs

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(clas Task1
+{
+    stat1c v0id Main(String[] args)
+    {
+        Console.WroteLine("Hi world'');
+    }
+}
